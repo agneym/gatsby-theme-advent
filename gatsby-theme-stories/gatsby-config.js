5a5c470b50e85ceed11f4a3d582c1835ddf40046
Boy with Silver Wings <agney@outlook.in>
2019-11-30 10:21:01 +0530

    add plugins to gatbsy config

--- gatsby-theme-stories/gatsby-config.js
+++ gatsby-theme-stories/gatsby-config.js
@@ -1,9 +1,32 @@
-module.exports = ({ contentPath = "data" }) => ({
+module.exports = ({ contentPath = "content" }) => ({
   plugins: [
+    `gatsby-plugin-styled-components`,
     {
       resolve: "gatsby-source-filesystem",
       options: {
         path: contentPath,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 600,
+              showCaptions: true,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+        ],
       },
     },
   ],
