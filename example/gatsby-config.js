60f8651f829e40f974d94e79d97c7ffe9389f362
Boy with Silver Wings <agney@outlook.in>
2019-11-30 10:15:58 +0530

    add gatsby theme stories to example

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  plugins: [{ resolve: `gatsby-theme-minimal`, options: {} }],
-}
+  plugins: [{ resolve: `@agney/gatsby-theme-stories`, options: {} }],
+};
--- gatsby-theme-stories/gatsby-config.js
+++ gatsby-theme-stories/gatsby-config.js
@@ -1,1 +1,10 @@
-module.exports = {};
+module.exports = ({ contentPath = "data" }) => ({
+  plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: contentPath,
+      },
+    },
+  ],
+});
